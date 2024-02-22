b97a164d4379f150652158295f791f8502bd7473
farkhanmhd <farkhanmuhammadv3@gmail.com>
2024-02-23 00:34:55 +0700

    initial commit. setup eslint, prettier, tailwind-auto, redux, redux-toolkit, react-icons

---
+++ src/app/components/Navigation/NavLink.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Link from "next/link";
+
+export default function NavLink({
+  href,
+  children,
+}: {
+  href: string;
+  children: React.ReactNode;
+}) {
+  return (
+    <li>
+      <Link
+        href={href}
+        className="block rounded-md px-5 py-3 duration-300 hover:bg-[rgba(70,_70,_70,_.7)]"
+      >
+        {children}
+      </Link>
+    </li>
+  );
+}
