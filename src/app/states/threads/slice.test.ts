b361004391187297377e574ea287c30473cf929f
farkhanmhd <farkhanmuhammadv3@gmail.com>
2024-03-11 11:56:30 +0700

    first unit test working

--- src/app/states/threads/slice.test.ts
+++ src/app/states/threads/slice.test.ts
@@ -1,6 +1,42 @@
+import threadSlice, { ThreadInterface } from "./slice";
+
 /**
  * test scenarios for thread slice
  *
- * should return new thread
- *
+ * should return a new thread
+ * should return a new comment
+ * should push a new userId to upVotesBy
+ * should push a new userId to comments upVotesBy
  */
+
+describe("thread slice", () => {
+  it("should return a new thread", () => {
+    const state: ThreadInterface[] = [];
+    const newThread: ThreadInterface = {
+      id: "1",
+      title: "test",
+      body: "test",
+      category: "test",
+      createdAt: new Date().toISOString(),
+      ownerId: "user-1",
+      upVotesBy: [],
+      downVotesBy: [],
+      totalComments: 0,
+      ownerName: "user-1",
+      avatar: "https://ui-avatars.com/api/?name=user&background=random",
+      comments: [],
+    };
+
+    const action = {
+      type: "threads/addNewThread",
+      payload: newThread,
+    };
+
+    const newState = threadSlice.reducer(state, action);
+
+    expect(newState).toEqual([newThread, ...state]);
+  });
+  it("should return a new comment", () => {});
+  it("should push a new userId to upVotesBy", () => {});
+  it("should push a new userId to comments upVotesBy", () => {});
+});
